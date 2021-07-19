182c31a2d97f5d64efbdd7ff389fa81127f142ea
Alyson Vilela <alysoonvila2@gmail.com>
2021-07-19 17:00:43 -0300

    :bug:fix: Lit fix

--- components/SeoTags.tsx
+++ components/SeoTags.tsx
@@ -15,7 +15,7 @@ type SeoSettings= {
       return (
           <>
           <meta charSet="UTF-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta name="description" content={desc} />
           <meta property="og:type" content="website" />
