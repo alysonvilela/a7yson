5864f8f7ce9a0183b7fb8574cbcccf4ba465399c
Alyson Vilela <alysoonvila2@gmail.com>
2021-07-19 22:27:38 -0300

    :bug:fix:About page init

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import Link from 'next/link'
 import { useRouter } from 'next/router';
+import Link from 'next/link'
+import Image from 'next/image';
+import firstIcon from '../../public/assets/memojis/hey.svg'
 import Head from 'next/head';
 import { SeoTags } from '../../components/SeoTags';
 
 
 const About = () => {
   const patternName = " - Alyson Vilela | Front-end Developer | UX/UI"
-  const title = "Page in Construction"
+  const title = "About"
   const description = "Page in Construction"
 
   return (
@@ -16,11 +18,36 @@ const About = () => {
     <title>{title} {patternName}</title>
     <SeoTags title={title} desc={description} img="imagem"/>
     </Head>
-    <div className="container error-page text-center">
-      <h1>👷‍♀️👷‍♂️</h1>
-      <h2>{title}</h2>
-      <p>Meet me at <a href="https://twitch.tv/a7yson"><span className="p p-purple">Twitch</span></a></p>
+    
+  <section id="introAbout">
+    <div className="container py-3 py-md-5">
+      <div className="row align-items-end justify-content-center">
+        <div className="cont-large col-lg-7 p-white">
+        <img src="./assets/memojis/hey.svg" alt="Alyson's character'"/>
+          {/* <Image src={firstIcon}
+                 alt="Icon Alyson, Hey!"
+                 layout="responsive"
+                    /> */}
+          <div className="miniContainer">
+            <h3>What I do ?</h3>
+            <ul>
+              <li>UX / UI designing</li>
+              <li>Front-end developing</li>
+              <li>Civil Engeneering</li>
+            </ul>
+            <a href="#" className="btn btn-red"> Hire me!</a>
+          </div>
+        </div>
+        <div className="intro-text col-lg-5">
+          <div className="text-part text-md-start">
+            <h1>Hey 👋</h1>
+            <p className="p-white">I'm so excited that you are here. Hope you like my mission and my work</p>
+          </div>
+        </div>
+      </div>
     </div>
+  </section>
+
     </>
   )
 }
